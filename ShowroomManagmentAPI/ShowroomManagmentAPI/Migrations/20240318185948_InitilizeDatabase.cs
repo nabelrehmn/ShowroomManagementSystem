40349525826120b1c048bb278a544d1ed6003382
MuhammadMustafa7022 <muhammadmustafa7022@gmail.com>
2024-03-19 00:01:43 +0500

    Database Created

---
+++ ShowroomManagmentAPI/ShowroomManagmentAPI/Migrations/20240318185948_InitilizeDatabase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShowroomManagmentAPI.Migrations
+{
+    public partial class InitilizeDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
