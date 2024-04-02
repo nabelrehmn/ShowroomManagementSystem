80f98c7a5f794068b847319031810923f037e021
Nabeel Rehman <136803583+nabelrehmn@users.noreply.github.com>
2024-04-02 07:40:25 +0500

    [Merged Changes Commit]

--- ShowroomManagmentAPI/ShowroomManagmentAPI/obj/Debug/net6.0/ShowroomManagmentAPI.AssemblyInfo.cs
+++ ShowroomManagmentAPI/ShowroomManagmentAPI/obj/Debug/net6.0/ShowroomManagmentAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShowroomManagmentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1fb825a1399d943362012ddc325ff0ee45d5537")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShowroomManagmentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShowroomManagmentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
